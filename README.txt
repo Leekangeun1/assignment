323991b4ce746801d7aa5a05cb89b32f99111518
Leekangeun <graceke1@naver.com>
2021-10-07 12:51:52 +0900

    Multiply

---
+++ README.txt
@@ -0,0 +1,7 @@
+The syntax for matrix.sh
+
+-> ./matrix.sh (int) (int)
+ex) ./matrix.sh 3 4
+
+The two parameters must be declared as integers over 0.
+If any integer under 1 is declared, "invalid parameter" sign will printed out.
--- matrix.sh
+++ matrix.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+if [ ${1} -lt 1 -o ${2} -lt 1 ] ; then
+  echo "Invalid Parameter"  
+fi
+
 for ((i=1; i<=${1}; i++)); do
-for ((j=1; j<=${2}; j++)); do
+  for ((j=1; j<=${2}; j++)); do
 
-let k=${i}*${j}
-printf "${i}x${j}="
-printf "${k} "
+    let k=${i}*${j}
+    printf "${i}x${j}="
+    printf "${k} "
 
-done
-echo ""
+  done
+  echo ""
 done
